4b00534e1928c186df08ed5ead942dbd8c62fc54
Gyumong <rlaalsrb3559@naver.com>
2020-05-09 22:08:12 +0900

    토큰 발급하기

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const UserSchema= new Schema({
     username: String,
@@ -16,9 +17,6 @@ UserSchema.methods.checkPassword= async function(password){
     return result; //true / false
 };
 
-UserSchema.statics.findByUsername= function(username){
-    return this.findOne({username});
-};
 
 UserSchema.methods.serialize = function(){
     const data= this.toJSON();
@@ -26,5 +24,23 @@ UserSchema.methods.serialize = function(){
     return data;
 }
 
-const User= mongoose.model('User',UserSchema);
-export default User;
+
+UserSchema.methods.generateToken = function(){
+    const token= jwt.sign(
+        // 첫 번째 파라미터에는 토큰 안에 집어넣고 싶은 데이터를 넣습니다.
+        {
+            _id:this.id,
+            username: this.username,
+        },
+        process.env.JWT_SECRET, // 두 번째 파라미터에는 JWT 암호를 넣습니다.
+        {
+            expiresIn:'7d', // 7일 동안 유효함
+        },
+        );
+        return token;
+    }
+            UserSchema.statics.findByUsername= function(username){
+                return this.findOne({username});
+            };
+    const User= mongoose.model('User',UserSchema);
+    export default User;
