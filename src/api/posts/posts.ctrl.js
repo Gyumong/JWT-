783683d12c95d31238262bd096771ad8822cedda
Gyumong <rlaalsrb3559@naver.com>
2020-05-22 16:07:16 +0900

    HTML 필터링하기

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,8 +1,33 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
 import Joi from 'joi';
+import sanitizeHtml from 'sanitize-html';
 
 const {ObjectId} = mongoose.Types;
+
+const sanitizeOption = {
+    allowedTags: [
+        'h1',
+        'h2',
+        'b',
+        'i',
+        'u',
+        's',
+        'p',
+        'ul',
+        'ol',
+        'li',
+        'blockquote',
+        'a',
+        'img',
+    ],
+    allowedAttributes:{
+        a:['href','name','target'],
+        img:['src'],
+        li:['class'],
+    },
+    allowedSchemes: ['data','http'],
+};
 
 export const getPostById= async(ctx, next)=>{
     const{id} =ctx.params;
@@ -52,7 +77,7 @@ export const write= async ctx=>{
     const {title, body,tags}= ctx.request.body;
     const post= new Post({
         title,
-        body,
+        body: sanitizeHtml(body,sanitizeOption),
         tags,
         user:ctx.state.user,
     });
@@ -63,8 +88,15 @@ export const write= async ctx=>{
         ctx.throw(500,e);
     }
 };
+// html을 없애고 내용이 너무 길면 200자로 제한하는 함수
+const removeHtmlAndShorten =body =>{
+    const filtered = sanitizeHtml(body,{
+        allowedTags:[],
+    });
+    return filtered.length <200 ? filtered: `${filtered.slice(0,200)}...`;
+}
 /*
-    GET /api/posts
+    GET /api/posts?username=&tag=&page=
 */
 export const list= async ctx=>{
     // query는 문자열이기 때문에 숫자로 변환해 주어야 합니다.
@@ -87,8 +119,7 @@ export const list= async ctx=>{
         ctx.set('Last-Page', Math.ceil(postCount/10));
         ctx.body=posts.map(post=>({
                 ...post,
-                body:
-                    post.body.length <200 ?post.body: `${post.body.slice(0,200)}...`
+                body: removeHtmlAndShorten(post.body),
             }));
     }catch(e){
         ctx.throw(500,e);
@@ -131,14 +162,13 @@ export const update=async ctx=>{
     });
 
     // 검증하고 나서 검증 실패인 경우 에러 처리
-    const result = Joi.validate(ctx.request.body, schema);
-    if(result.error){
-        ctx.status= 400; // Bad Request
-        ctx.body= result.error;
-        return;
-    }
+  const nextData = {...ctx.request.body}; // 객체를 복사하고
+  // body 값이 주어졌으면 HTML 필터링
+  if(nextData.body){
+      nextData.body=sanitizeHtml(nextData.body);
+  }
     try{
-        const post =await Post.findByIdAndUpdate(id,ctx.request.body,{
+        const post =await Post.findByIdAndUpdate(id,nextData,{
             new:true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
             // false일 때는 업데이트되기 전의 데이터를 반환합니다.
         }).exec();
